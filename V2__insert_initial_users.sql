e0049f5ae8a9aed0748e92d86f691929c3666026
cherukuri333 <96185030+cherukuri333@users.noreply.github.com>
2025-05-26 14:47:17 +0530

    Create V2__insert_initial_users.sql

---
+++ V2__insert_initial_users.sql
@@ -0,0 +1,6 @@
+-- V2__insert_initial_users.sql
+
+INSERT INTO users (username, email) VALUES
+  ('alice', 'alice@example.com'),
+  ('bob', 'bob@example.com'),
+  ('charlie', 'charlie@example.com');
