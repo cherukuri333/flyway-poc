55d6f3df4290d2e6840562b4d89f4632e71ae3b1
cherukuri333 <96185030+cherukuri333@users.noreply.github.com>
2025-05-26 13:57:54 +0530

    Update V1__init.sql

--- V1__init.sql
+++ V1__init.sql
@@ -1,1 +1,8 @@
+-- V1__init.sql
 
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
